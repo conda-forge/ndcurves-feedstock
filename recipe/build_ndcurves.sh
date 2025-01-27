a3dab812cca8421ec3c8de49c12d38c58f8465ae
Joris Vaillant <joris.vaillant@inria.fr>
2025-01-27 14:15:37 +0100

    Add numpy

--- recipe/build_ndcurves.sh
+++ recipe/build_ndcurves.sh
@@ -5,12 +5,28 @@ cd build
 
 echo "TESTEST $PYTHON"
 
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+  export Python3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS
+else
+  export Python3_NumPy_INCLUDE_DIR=$BUILD_NUMPY_INCLUDE_DIRS
+fi
+
 cmake ${CMAKE_ARGS} .. \
       --trace \
       --trace-expand \
       -GNinja \
       -DCMAKE_BUILD_TYPE=Release \
       -DPYTHON_EXECUTABLE=$PYTHON \
+      -DPython3_NumPy_INCLUDE_DIR=$Python3_NumPy_INCLUDE_DIR \
       -DBUILD_DOCUMENTATION=OFF \
       -DBUILD_PYTHON_INTERFACE=OFF \
       -DGENERATE_PYTHON_STUBS=OFF \
